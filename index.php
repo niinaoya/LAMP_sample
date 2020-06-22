382647ba8edaf6d40c212b2c0b46548ba90e7ad6
niinaoya <n.niinuma.sys19@morijyobi.ac.jp>
2020-06-22 10:13:10 +0900

    add index.php 2

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
-$dsn = 'mysql:dbname=user;host=127.0.0.1;';
-$user = 'ubuntumysqlnnaoya';
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'otsubo';
 $password = 'morijyobi';
 try {
     $dbh = new PDO($dsn, $user, $password);
-    $sql = "select * from user"
+​
+    $sql = "select * from user";
     $result = $dbh->query($sql);
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
 }
 ?>
-
-<!DOCTYPE HTML>
+​
+<!DOCTYPE html>
+<html lang="ja">
+​
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LAMP SAMPLE PAGE</title>
 </head>
+​
 <body>
-<div class="container">
-    <?php foreach()($result as $value){
-        echo "<h1>$value[id] $value[name] $value[age]</h1>";
-    }?>
-</div>
+    <div class="container">
+        <?php foreach ($result as $value) {
+            echo "<h1>$value[id] $value[name] $value[age]</h1>";
+        } ?>
+    </div>
 </body>
+​
+</html>
